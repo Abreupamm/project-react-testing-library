49c9f265c0ff6e877ca619178bce1f3055795c7c
pamela <abreupamm@gmail.com>
2022-06-06 20:33:46 -0300

    questão 4 feita

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+
+describe('Testando o componente NotFound.js', () => {
+  beforeEach(() => {
+    render(<NotFound />);
+  });
+  test('Verifica se a página possui o texto correto', () => {
+    const textElement = screen.getByRole(
+      'heading', { name: /Page requested not found/i, level: 2 },
+    );
+    expect(textElement).toBeInTheDocument();
+  });
+  test('Verifica se a página mostra a imagem correta', () => {
+    const imgElemnent = screen.getByRole(
+      'img', { name: /Pikachu crying because the page requested was not found/i },
+    );
+    expect(imgElemnent.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
