05657bb18fa49e212a776f58f04613b40671897a
pamela <abreupamm@gmail.com>
2022-06-06 19:02:57 -0300

    questão 2 feita

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -12,8 +12,10 @@ describe('Testando o componente About.js', () => {
     );
     const info1 = screen.getByText(/This application simulates a Pokédex/i);
     const info2 = screen.getByText(/One can filter Pokémons by type/i);
+    const imgElemnent = screen.getByRole('img', { name: 'Pokédex' });
     expect(aboutTitleElement).toBeInTheDocument();
     expect(info1).toBeInTheDocument();
     expect(info2).toBeInTheDocument();
+    expect(imgElemnent.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
 });
