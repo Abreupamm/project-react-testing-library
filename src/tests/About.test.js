382cce5f4334169b7632ea959c5149ee3069c1d0
pamela <abreupamm@gmail.com>
2022-06-06 18:46:53 -0300

    test h2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,15 @@
-test(' ', () => {});
+import { screen, render } from '@testing-library/react';
+import React from 'react';
+import About from '../pages/About';
+
+describe('Testando o componente About.js', () => {
+  beforeEach(() => {
+    render(<About />);
+  });
+  test('Verifica se a plicação possui informações sobre a Pokédex', () => {
+    const aboutTitleElement = screen.getByRole(
+      'heading', { name: /About Pokédex/i, level: 2 },
+    );
+    expect(aboutTitleElement).toBeInTheDocument();
+  });
+});
