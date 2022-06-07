82d0d61bc853b0e7a0cf9ba9ef7010b66cf564e6
pamela <abreupamm@gmail.com>
2022-06-07 15:30:19 -0300

    botão 'próximo pokémon' testado

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -3,15 +3,20 @@ import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
+import { element } from 'prop-types';
 import App from '../App';
+// import Pokedex from '../pages';
+import pokemons from '../data';
 
 describe('Testando o componente Pokedex.js', () => {
-  const history = createMemoryHistory();
-  render(
-    <Router history={ history }>
-      <App />
-    </Router>,
-  );
+  beforeEach(() => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+  });
   test('Verifica se existe o texto "Encountered pokémons"', () => {
     const titleElement = screen.getByRole(
       'heading', { name: 'Encountered pokémons', level: 2 },
@@ -21,28 +26,21 @@ describe('Testando o componente Pokedex.js', () => {
   test(
     'Verifica  se é exibido o próximo pokémon da lista quando o botão clicado',
     () => {
-      const listCards = [
-        'Pikachu',
-        'Charmander',
-        'Caterpie',
-        'Ekans',
-        'Ekans',
-        'Mew',
-        'Rapidash',
-        'Snorlax',
-        'Dragonair',
-      ];
-      const buttonElement = screen.getAllByRole('button', { name: /próximo pokémon/i });
-      listCards.forEach((el, index) => {
-        if (index === listCards.length - 1) {
+      const buttonElement = screen.getByRole(
+        'button', { name: 'Próximo pokémon', hidden: true },
+      );
+      pokemons.forEach((el, index) => {
+        if (index === pokemons.length - 1) {
           userEvent.click(buttonElement);
           const cardElement1 = screen.getByTestId('pokemon-name');
-          expect(cardElement1).toBe(listCards[0]);
+          const pokemon = pokemons[0];
+          expect(cardElement1.innerHTML).toBe(pokemon.name);
         } else {
           const i = index + 1;
           userEvent.click(buttonElement);
-          const cardElement1 = screen.getByTestId('pokemon-name');
-          expect(cardElement1).toBe(listCards[i]);
+          const cardElement2 = screen.getByTestId('pokemon-name');
+          const pokemon = pokemons[i];
+          expect(cardElement2.innerHTML).toBe(pokemon.name);
         }
       });
     },
